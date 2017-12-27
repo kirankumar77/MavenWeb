840a1ef0ee788a31e0a4ccaa2c7a5c6633af1c2e
dodla suneel <suneel.dodla@kohls.com>
2017-12-28 01:40:28 +0530

    testing

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Welcommmmmmmmmmmmmmmmmmmme");
+		model.addAttribute("message", "Welme");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
