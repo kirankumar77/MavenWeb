cdede0e281acafe406c4d37b34d7e0b4928151dc
dodla suneel <suneel.dodla@kohls.com>
2017-12-28 16:49:27 +0530

    hgfds

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Wennnnnnnlme");
+		model.addAttribute("message", "Welme");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
